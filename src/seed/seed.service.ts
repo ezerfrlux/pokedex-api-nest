e2265a9cb23846f18745ac08f4b749a502158f6c
ezerfrlux <aaron.salinas318@gmail.com>
2026-01-15 12:55:03 -0600

    [UPDATE] - Refactor SeedService to improve structure and add CreatePokemonDto; update PokemonModule exports

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,21 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { PokemonService } from 'src/pokemon/pokemon.service';
+import { CreatePokemonDto } from 'src/pokemon/dto/create-pokemon.dto';
 
 @Injectable()
 export class SeedService {
+  private readonly axios: AxiosInstance = axios;
 
-  private readonly axios:AxiosInstance = axios;
+  constructor(private readonly pokemonService: PokemonService) {}
+
   async executeSeed() {
-    const {data} = await  this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10');
-    
-    data.results.forEach(({name, url}) => {
+    const { data } = await this.axios.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=10',
+    );
+
+     data.results.forEach(({ name, url }) => {
       const segments = url.split('/');
-      const no:number = +segments[segments.length -2];
+      const no: number = +segments[segments.length - 2];
+      const pokemonDto: CreatePokemonDto = { name, no };
+
+      this.pokemonService.create(pokemonDto);
     });
-    
-    return data.results;
+
+    return "Seed executed"
   }
 }
-
-
