9280b72b14e53acc32fc03096b49420c81c6fb50
ezerfrlux <aaron.salinas318@gmail.com>
2026-01-17 10:52:26 -0600

    [fix errror]

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,9 +20,7 @@ import { JoiValidationSchema } from './config/joi.validation';
       rootPath: join(__dirname, '..', 'public'),
     }),
 
-    MongooseModule.forRoot(process.env.MONGODB!, {
-      dbName: "pokemonsDb"
-    }),
+    MongooseModule.forRoot(process.env.MONGODB!),
 
     PokemonModule,
 
